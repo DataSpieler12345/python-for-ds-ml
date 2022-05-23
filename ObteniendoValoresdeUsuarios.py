3e50cc8e7ca0cf036300b56c00ba2c2ea4615ba8
BI - Analytics Consultant <45371372+DataSpieler12345@users.noreply.github.com>
2022-05-23 14:58:42 +0200

    Add files via upload

---
+++ ObteniendoValoresdeUsuarios.py
@@ -0,0 +1,15 @@
+#Obteniendo los valores de Usuarios
+
+nombre = input("dime tu nickname ")
+print("Hola, "+ nombre, end=" ")
+print("Hola, ",nombre+"\n\n\n")
+print(f"Hola, {nombre}"+"\t\t\testo esta separado por tabulador")
+
+#Solicitando numeros 
+numero1 = int(input("Dime tu mejor numero "))
+numero2 = int(input("Dime un segundo numero "))
+resultado = numero1 + numero2
+print(f"La suma de {numero1} mas {numero2} es igual a {resultado}")
+ 
+ 
+
