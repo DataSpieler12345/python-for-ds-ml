57c7d9b65960425c3ccb4210e96844d59f1cd0cb
BI - Analytics Consultant <45371372+DataSpieler12345@users.noreply.github.com>
2023-02-27 09:13:12 +0100

    Delete Case Study_Binary Disorder.py

--- Case Study_Binary Disorder.py
+++
@@ -1,9 +0,0 @@
-y_true = [int(x) for x in input().split()]
-y_pred = [int(x) for x in input().split()]
-
-from sklearn.metrics import confusion_matrix
-import numpy as np
-
-cm = confusion_matrix(y_pred, y_true, labels = [1,0])
-
-print(np.array(cm, dtype ='f'))
