f55161c2291d2a476b3f0765f3658412e6bbf759
BI - Analytics Consultant <45371372+DataSpieler12345@users.noreply.github.com>
2023-02-27 09:13:54 +0100

    Delete Case Study_Missing Numbers.py

--- Case Study_Missing Numbers.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-lst = [float(x) if x != 'nan' else 
-       np.NaN for x in input().split()]
-arr=np.asarray(lst)
-pd=pd.Series(arr)
-p=pd.fillna(pd.mean().round(1))
-print(p)
