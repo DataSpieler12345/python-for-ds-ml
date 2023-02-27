101612ed0936471f1a292fde7ade035c94862f48
BI - Analytics Consultant <45371372+DataSpieler12345@users.noreply.github.com>
2023-02-27 09:15:09 +0100

    Delete Modulo_02.py

--- Modulo_02.py
+++
@@ -1,8 +0,0 @@
-#Importando del modulo random una funcion
-from random import randint as azar 
-continua = "s"
-while(continua == "s" or continua == "S"):
-   lanzaDado = azar(1,6)
-   print("Has sacado un "+str(lanzaDado))
-   continua = input("Continuamos(s/n)? ")
-print("Ya no tiro mas el dado, hasta luego")
