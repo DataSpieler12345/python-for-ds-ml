5d74488496d0f564850dd5c40debfc0edcfed5d8
BI - Analytics Consultant <45371372+DataSpieler12345@users.noreply.github.com>
2023-02-27 09:11:38 +0100

    Delete Case Study_Average of Rows.py

--- Case Study_Average of Rows.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-n, p = [int(x) for x in
-input().split()]
-list = []
-for i in range(n):
-   list.append(input().split())
-print(np.array(list).astype(np.float16).mean(axis=1).round(2))
