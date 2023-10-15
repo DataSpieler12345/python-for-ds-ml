35bdd351818232e57a73c79ced495e06ab0b241d
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2023-10-15 16:50:05 +0200

    commit py-bildung

---
+++ python for non-mathematicians/loops and lists/exercise for loop.py
@@ -0,0 +1,10 @@
+# exercise for loop
+# create a program that displays the sum of all the numbers between 0 and 100
+
+total = 0
+for i in range(101):
+   total=total+i
+   print("summation:", total)
+
+
+ 
---
+++ python for non-mathematicians/loops and lists/for loop.py
@@ -0,0 +1,4 @@
+# for loop
+data = [6,8,9,4,"hi"]
+for i in data:
+   print(f"object: {i}")
