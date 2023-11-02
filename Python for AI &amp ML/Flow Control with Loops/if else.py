eff58af256916f9c953152fc4e7240038142beef
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2023-11-02 16:26:11 +0100

    commit -py-bildung

---
+++ Python for AI &amp ML/Flow Control with Loops/if else.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center><h1><b><font size="6">If / Else</font></b></h1></center>
+
+# In[1]:
+
+
+# printing characteristics of a number
+# determining if a number if positive or negative 
+
+number = 7
+
+if number  >= 0:
+    print("Positive Number")
+else:
+    print("Negative Number")
+
+
+# In[2]:
+
+
+# printing characteristics of a number
+# determining if a number if positive or negative 
+
+n = float(input("What's your number ?"))
+
+if n  >= 0:
+    print("Positive Number")
+else:
+    print("Negative Number")
+
+
+# In[3]:
+
+
+# printing characteristics of a number
+# determining if a number if positive or negative 
+
+x = float(input("What's your number ?"))
+
+if x  >= 0:
+    squared = x**2
+    print(squared)
+else:
+    print("Negative Number")
+
+
+# In[ ]:
+
+
+
+
