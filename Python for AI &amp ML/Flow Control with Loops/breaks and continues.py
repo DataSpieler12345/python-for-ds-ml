9f0f77abb667e5dc19a760bfed3aa969057ec78d
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2023-11-02 18:23:58 +0100

    commit py-bildung

---
+++ Python for AI &amp ML/Flow Control with Loops/breaks and continues.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center><h1><b><font size="6">Breaks & Continues</font></b></h1></center>
+
+# In[2]:
+
+
+for x in "this string here":
+    if x == "h":
+    	break
+    print(x)
+    
+print("this ends our programm")
+
+
+# In[3]:
+
+
+for x in "this string here":
+    if x == "h":
+    	continue
+    print(x)
+    
+print("this ends our programm")
+
+
+# In[ ]:
+
+
+
+
